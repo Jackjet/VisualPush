1ef1c39915189193e11ae23436d9e750ea90f79c
acasquete <acasquete@outlook.com>
2014-02-25 23:27:47 +0100

    First commit

---
+++ VisualPush.W8/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Practices.Prism.StoreApps;
+
+namespace VisualPush.W8.Views
+{
+    public sealed partial class MainPage : VisualStateAwarePage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
