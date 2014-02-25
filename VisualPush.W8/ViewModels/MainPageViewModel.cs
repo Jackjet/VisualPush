1efa81f89765ca28931aa54273c3759459638ad6
acasquete <acasquete@outlook.com>
2014-02-26 00:40:02 +0100

    Added basic fields

--- VisualPush.W8/ViewModels/MainPageViewModel.cs
+++ VisualPush.W8/ViewModels/MainPageViewModel.cs
@@ -7,9 +7,13 @@ namespace VisualPush.W8.ViewModels
     {
         private INavigationService _navigationService;
 
+        public string Payload { get; set; }
+
         public MainPageViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+            Payload = @"<tile><visual version=""2""><binding template=""TileWide310x150ImageAndText01"" fallback=""TileWideImageAndText01""><image id=""1"" src=""http://tokiota.com/images/tiles/widetile-tokiota-310-b.jpg"" alt=""alt text""/><text id=""1"">Tokiota Pawa!</text></binding></visual></tile>";
+            
         }
     }
 }
